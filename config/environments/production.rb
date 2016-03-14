79fb6e02d96c2330bf9bb07b145405ce4056df95
Tien Do <domanhtien2011@gmail.com>
2016-03-14 14:37:44 +0700

    edit production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,10 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = true
+   config.action_mailer.perform_deliveries = true
+  config.action_mailer
+  raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
